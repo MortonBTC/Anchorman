8207f2b593f1f8edae7bd370ea5bbf03e61dcecc
jbhurley92 <jbhurley92@gmail.com>
2016-04-27 10:28:20 -0400

    app.js done

--- js/app.js
+++ js/app.js
@@ -1,40 +1,65 @@
+/* jslint esnext: true */
 var app = angular.module('AnchormanApp', ['ngRoute']);
 
 app.config(['$routeProvider', function ($routeProvider) {
-    $routeProvider.when('/list', {
-        controller: 'ListController',
-        templateUrl: 'templates/list.html',
-    }).when('/interests', {
-        controller: 'InterestsController',
-        templateUrl: 'templates/interests.html',
-    }).when('/saved', {
-        controller: 'SavedController',
-        templateUrl: 'templates/saved.html',
-    });
+   $routeProvider
+      .when('/home', {
+         controller: 'HomeController',
+         templateUrl: 'templates/home.html'
+      })
+      .when('/interests', {
+         controller: 'InterestsController',
+         templateUrl: 'templates/interests.html'
+      })
+      .when('/saved', {
+         controller: 'SavedController',
+         templateUrl: 'templates/saved.html'
+      })
+      .otherwise({
+         redirectTo: '/home'
+      });
 }]);
 
+// controller main page - news stream
+app.controller('HomeController', ['$scope', '$location', '$window', 'NewsService', function ($scope, $location, $window, NewsService) {
 
-app.controller('ListController', ['$scope', 'StoriesService', function($scope, StoriesService) {
-    
-    
+   NewsService.getNews().then(function (data) {
+      console.log(data.data.stories);
+      $scope.stories = data.data.stories;
+   });
+
+   // open new tab navigate to vendor for full article
+   $scope.getStory = function (story) {
+      $window.open(`${story.link}`, '_blank');
+   };
+
 }]);
 
-app.controller('InterestsController', ['$scope', 'StoriesService', function($scope, StoriesService) {
-    
-    
+app.controller('InterestsController', ['$scope', '$location', 'NewsService', function ($scope, $location, NewsService) {
+   //for 'interests' view
 }]);
 
-app.controller('SavedController', ['$scope', 'StoriesService', function($scope, StoriesService) {
-    
-    
+app.controller('SavedController', ['$scope', '$location', 'NewsService', function ($scope, $location, NewsService) {
+   // for 'saved' view
 }]);
 
+app.factory('NewsService', function ($http) {
 
+   let news = $http({
+      method: 'GET',
+      url: 'http://chat.queencityiron.com/api/news/latest'
+   }).then(function (response) {
+      return response;
+   });
 
-app.factory('StoriesService', function() {
-    var stories = [];
-    
-    return {
-        
-    };
+   return {
+      getNews: function () {
+         return news;
+      }
+   };
+   // publisher data that will likely connect us to our article
+   // 'http://chat.queencityiron.com/api/publishers'
+
+   // websockets
+   // ws://chat.queencityiron.com/api/feed
 });
