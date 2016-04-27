76b0091139133f997faf3bd692c0aeb3c8269f8a
jbhurley92 <jbhurley92@gmail.com>
2016-04-27 10:13:08 -0400

    Api working! Working View! Changed HTML to show view Added HTML Templates!

--- js/app.js
+++ js/app.js
@@ -1,126 +1,65 @@
-var angular = require('angular');
-var angularRoute = require('angular-route');
-require('./filters');
-var app = angular.module('NewsFeedApp', ['ngRoute', 'publisherName']);
+/* jslint esnext: true */
+var app = angular.module('AnchormanApp', ['ngRoute']);
 
-
-
-app.filter('reverse', function() {
-  return function(items) {
-    return items.slice().reverse();
-  };
-});
-
-/*Create a router*/
 app.config(['$routeProvider', function ($routeProvider) {
-    $routeProvider
-        .when('/feed', {
-            controller: 'FeedViewController',
-            templateUrl: 'sections/feed.html'
-        })
-        .when('/interests', {
-            controller: 'InterestViewController',
-            templateUrl: 'sections/interests.html',
-        })
-        .when('/saved', {
-            controller: 'SavedViewController',
-            templateUrl: 'sections/saved.html',
-        })
-        .otherwise({
-            redirectTo: '/feed',
-        });
+   $routeProvider
+      .when('/home', {
+         controller: 'HomeController',
+         templateUrl: 'templates/home.html'
+      })
+      .when('/interests', {
+         controller: 'InterestsController',
+         templateUrl: 'templates/interests.html'
+      })
+      .when('/saved', {
+         controller: 'SavedController',
+         templateUrl: 'templates/saved.html'
+      })
+      .otherwise({
+         redirectTo: '/home'
+      });
 }]);
 
+// controller main page - news stream
+app.controller('HomeController', ['$scope', '$location', '$window', 'NewsService', function ($scope, $location, $window, NewsService) {
 
-app.controller('FeedViewController', ['$scope', '$http', 'NewsService', function ($scope, $http, NewsService) {
-    console.log('FeedViewController View');
+   NewsService.getNews().then(function (data) {
+      console.log(data.data.stories);
+      $scope.stories = data.data.stories;
+   });
 
-    /*$scope.articles is the array of the articles*/
-    $scope.articles = NewsService.getArticles();
-
-    /*$scope.saveArticle is run when the save button is clicked.*/
-    $scope.saveArticle = function (article) {
-        console.log('Article save button clicked');
-        console.log(article.title);
-        NewsService.saveArticle(article);
-    };
-
-       $scope.publisher = NewsService.getPublishers();
-}]);
-
-
-/*Create a controller for the Interests page*/
-app.controller('InterestViewController', ['$scope', '$http', 'NewsService', function ($scope, $http, NewsService) {
-    console.log('InterestViewController View');
-
-    $scope.saveInterest = function (interest) {
-      console.log('Interest saved!');
-    };
-}]);
-
-/*Create a controller for the Saved page*/
-app.controller('SavedViewController', ['$scope', '$http', 'NewsService', function ($scope, $http, NewsService) {
-    console.log('SavedViewController View');
-    $scope.articles = NewsService.getSavedArticles();
+   // open new tab navigate to vendor for full article
+   $scope.getStory = function (story) {
+      $window.open(`${story.link}`, '_blank');
+   };
 
 }]);
 
+app.controller('InterestsController', ['$scope', '$location', 'NewsService', function ($scope, $location, NewsService) {
+   //for 'interests' view
+}]);
 
+app.controller('SavedController', ['$scope', '$location', 'NewsService', function ($scope, $location, NewsService) {
+   // for 'saved' view
+}]);
 
-/*Create a service to store the data*/
 app.factory('NewsService', function ($http) {
 
-    let stories = [];
-    let savedStories = [];
-    let publishers = [];
-    let interests = [];
+   let news = $http({
+      method: 'GET',
+      url: 'http://chat.queencityiron.com/api/news/latest'
+   }).then(function (response) {
+      return response;
+   });
 
-    /*Any data we need to store can go in here.*/
-    $http({
-            method: 'get',
-            url: 'http://chat.queencityiron.com/api/news/latest',
-        })
-        .then(function (response) {
-            console.log('Response of stories');
-            console.log(response);
-            angular.copy(response.data.stories, stories);
-        });
+   return {
+      getNews: function () {
+         return news;
+      }
+   };
+   // publisher data that will likely connect us to our article
+   // 'http://chat.queencityiron.com/api/publishers'
 
-
-        if (publishers.length === 0) {
-            $http({
-                 method: 'get',
-                    url: 'http://chat.queencityiron.com/api/publishers',
-                })
-                .then(function (response) {
-                    publishers.push(response.data.providers);
-                })
-                .then(function () {
-                    console.log('Publisher information saved');
-                   console.log(publishers);
-               });
-        }
-
-    return {
-        getArticles: function () {
-            return stories;
-        },
-        saveArticle: function (article) {
-            /*Write code to push the saved article to an array.*/
-            savedStories.push(article);
-            console.log('article saved.');
-        },
-        getSavedArticles: function () {
-            return savedStories;
-        },
-        /*Not yet calling this function.*/
-        getPublishers: function () {
-            console.log('GetPublishers');
-            return publishers;
-        },
-        saveInterest: function (interest) {
-
-        }
-    };
-
+   // websockets
+   // ws://chat.queencityiron.com/api/feed
 });
